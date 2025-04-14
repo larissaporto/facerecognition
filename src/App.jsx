0f804e9211d2fdab0be0215ffe87ad9629e29152
Larissa Porto <larissa.silva86@gmail.com>
2025-04-14 16:10:12 +0200

    Add Image and the bronken fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,66 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import ParticlesBg from 'particles-bg'
 import './App.css'
+
+const returnClarifyOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = 'f165243b824e4e70be226dc52385253c';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'larissaporto';
+  const APP_ID = 'face-model';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions
+}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       input: '',
+      imageUrl: null
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value)
+    this.setState({ input: event.target.value })
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    this.setState({ imageUrl: this.state.input })
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifyOptions(this.state.input))
+      .then(response => response.json())
   }
 
   render() {
@@ -32,8 +75,8 @@ class App extends Component {
           <Rank />
           <ImageLinkForm
             onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}/>
-          {/* <FaceRecognition /> */}
+            onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition imageUrl={ this.state.imageUrl } />
         </div>
       </>
     )
