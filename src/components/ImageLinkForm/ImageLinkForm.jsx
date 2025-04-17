270d475a149a5c64000512f6dfbd008a3b9b6e4d
Larissa Porto <larissa.silva86@gmail.com>
2025-04-17 15:28:18 +0200

    Prettier

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import './ImageLinkForm.css'
+import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <>
-      <p className='f3 center'>
-        {'This Smart Brain will detect faces in the pictures Give it a try:'}
+      <p className="f3 center">
+        {"This Smart Brain will detect faces in the pictures Give it a try:"}
       </p>
-      <div className='center'>
-        <div className='pa4 br3 shadow-5 form center'>
-          <input className='f4 w-70 center' type='text' onChange= {onInputChange}/>
+      <div className="center">
+        <div className="pa4 br3 shadow-5 form center">
+          <input
+            className="f4 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
           <button
-            className='w-30 grow f4 link pv2 dib white bg-light-blue'
+            className="w-30 grow f4 link pv2 dib white bg-light-blue"
             onClick={onButtonSubmit}
-          >Detect</button>
+          >
+            Detect
+          </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ImageLinkForm
+export default ImageLinkForm;
--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Rank = () => {
   return (
     <>
-      <div className='white f3'>
-        {'Fulano, your current rank is...'}
-      </div>
-      <div className='white f1'>
-        {'#5'}
-      </div>
+      <div className="white f3">{"Fulano, your current rank is..."}</div>
+      <div className="white f1">{"#5"}</div>
     </>
-  )
-}
+  );
+};
 
-export default Rank
+export default Rank;
